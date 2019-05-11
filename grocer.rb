1cbc4a64d91c7c9def416920f65c6f4ed2d60af5
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-11 23:21:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,13 +18,14 @@ def apply_coupons(cart, coupons)
 coupons.each do |coupon|
 name = coupon[:item]
 if cart[name] && cart[name][:count] >= coupon[:num]
+   cart[name][:count] -= coupon[:num]
   if cart["#{name} W/COUPON"]
     cart["#{name} W/COUPON"][:count] += 1
   else
     cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
     cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
   end
-  cart[name][:count] -= coupon[:num]
+ 
 end
 end
 cart
@@ -45,7 +46,7 @@ end
     if (cart.keys.include?(k)  && hash_cart[k][:count] >= h_coup[:num])
       
       
-      hash_cart[k][:count]-=h_coup[:num]
+     hash_cart[k][:count]-=h_coup[:num] 
       
       hash_coup = hash_coup.merge(hash_cart[k])
       hash_cart_coup[k] = hash_cart_coup.merge(hash_cart[k]) 
